d2d4f9331df46bef3d8ac533e113ecd721add53f
basovart000 <basovart000@CAM-LAB-09.bgsd.k12.wa.us>
2019-09-16 07:54:09 -0700

    Incomplete.

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,73 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	public static void John() {
+		System.out.println("Dear John,");
+		Common();
+		System.out.println("I heard you got dragged back into mobile games");
+		System.out.println("You see an ad for a game and without");
+		System.out.println("a thought you download it and get dragged");
+		System.out.println("into a never ending cycle of download and play.");
+		System.out.println("Last time this happened I didn't see you for months.");
+		Sincerely();
+	}
+	public static void Hefflin() {
+		System.out.println("Dear Hefflin,");
+		Common();
+		System.out.println("How are you? I hear you are still sleep deprived.");
+		System.out.println("You need to get your life together, you need to not");
+		System.out.println("spend all of your time on sports and school. You need");
+		System.out.println("to take a break every once i na while.");
+		Sincerely();
+	}
+	public static void Alex() {
+		System.out.println("Hello Big brain Boi. Did you ");
+	}
+	public static void Sincerely() {
+		System.out.println("				Sincerely, Artyom");
+		System.out.println("");
+	}
+	public static void Common() {
+		System.out.println("	How's school? I've been having a decent start to the");
+		System.out.println("school year. Most of the classes are boring but CS is");
+		System.out.println("a lot of fun. Can't wait to learn how to hack into the");
+		System.out.println("government and be on the run for the rest of my life!");
+		System.out.println("Anyways, how's your day going?");
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Disclaimer: This is a joke!
